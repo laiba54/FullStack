45e2f73f75886bc9b3968c4acfa2c9627bb35968
laiba54 <152349107+laiba54@users.noreply.github.com>
2024-08-28 17:33:28 +0500

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 async function handleLoginFunction(req, res) {
   const { email, password } = req.body;
   try {
-      const user = await User.findOne({ email });
+      const user = await User.findOne({ email }).exec();
       if (!user) {
           return res.status(401).send({ msg: 'Invalid email or password' });
       }
