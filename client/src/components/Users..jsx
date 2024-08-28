0d898a8ecd304261f7214bd6418caa407410c5d8
laiba54 <152349107+laiba54@users.noreply.github.com>
2024-08-28 17:07:13 +0500

    Update Users..jsx

--- client/src/components/Users..jsx
+++ client/src/components/Users..jsx
@@ -9,7 +9,7 @@ const Users = () => {
 
   useEffect(() => {
     axios
-      .get("https://full-stack-8lwk-server.vercel.app/getallusers")
+      .get("https://full-stack-server-two.vercel.app/getallusers")
       .then((response) => {
         if (response.data.length === 0) {
           navigate("/");
@@ -35,7 +35,7 @@ const Users = () => {
               >
                 <div className="mt-2 d-flex justify-content-center">
                   <img
-                    src={`https://full-stack-8lwk-server.vercel.app/uploads/${user.profile}`}
+                    src={`https://full-stack-server-two.vercel.app/uploads/${user.profile}`}
                     width={80}
                     height={80}
                     style={{ borderRadius: "50%" }}
